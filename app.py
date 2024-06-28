f24a8a3bebfff2760e71bb0ecec11a8ce48b9189
nidhesg <nidheshruhela02@gmail.com>
2024-06-28 18:51:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # home route that returns below text when root url is accessed
 @app.route("/")
 def hello_world():
-	return "<p>Hello, Worlddsssss!</p>"
+	return "<p>Hello, World!</p>"
 
 if __name__ == '__main__': 
    app.run()
